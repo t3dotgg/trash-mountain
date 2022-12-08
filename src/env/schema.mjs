28e940143bdfa0566d53d828ea19dbfdf9217baf
Theo Browne <me@t3.gg>
2022-12-07 16:03:12 -0800

    no db around here

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,7 +6,7 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url().optional(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
